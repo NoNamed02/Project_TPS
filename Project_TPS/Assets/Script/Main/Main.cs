78524f171fded14a7485be5c7fe574ee86ff52d4
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-12-08 22:44:21 +0900

    feat : 메인 화면 매니저

---
+++ Project_TPS/Assets/Script/Main/Main.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject Loading;
+
+    public void StartButton()
+    {
+        Loading.SetActive(true);
+    }
+}
