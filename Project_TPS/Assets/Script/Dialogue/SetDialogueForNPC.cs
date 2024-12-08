f3bdc0811222799efad06ed830f9aca41b91ba81
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-12-08 22:43:15 +0900

    feat : NPC 상태 확인용 스크립트

---
+++ Project_TPS/Assets/Script/Dialogue/SetDialogueForNPC.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDialogueForNPC : MonoBehaviour
+{
+    public string conversationName;
+    public bool doTalk = false;
+}
