482c3ec52a70e50b8e917f8345c573cb258dc3d6
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-12-08 22:44:00 +0900

    feat : 보스 등장용 더미 인형 제거 스크립트

---
+++ Project_TPS/Assets/Script/Enemy/DummyBoss.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyBoss : MonoBehaviour
+{
+    public GameObject Boss;
+    public float spwanTime = 3f;
+    void Start()
+    {
+        StartCoroutine(BossSpwan(spwanTime));
+    }
+    IEnumerator BossSpwan(float spwanTime)
+    {
+        yield return new WaitForSeconds(spwanTime);
+        Boss.SetActive(true);
+        StartDialogue Player = GameObject.FindWithTag("Player").GetComponent<StartDialogue>();
+        Player.TalkEnd();
+        Destroy(gameObject);
+    }
+}
